288ed1ca15e044ffd569baea903ea20dd65722fa
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-04-21 22:53:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 function scuberGreetingForFeet(someValue) {
   let feet
   if (feet <= 400) {
-    message = "This one is on me!";
-  } else if (1999 < message && message < 2500) {
+    feet = "This one is on me!";
+  } else if (1999 < feet && message < 2500) {
     message = "I will gladly take your thirty bucks.";
   } else {
     message = "No can do.";
