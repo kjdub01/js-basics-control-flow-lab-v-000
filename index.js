3ba498985a856c4f5f4da6a69541c467db636bf6
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-04-21 23:24:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 // Write your code in this file!
 function scuberGreetingForFeet(someValue) {
-  let message;
   if (someValue <= 400) {
-    message = "This one is on me!";
+    return = "This one is on me!";
   } else if (1999 < someValue && someValue < 2500) {
-    message = "I will gladly take your thirty bucks.";
+    return = "I will gladly take your thirty bucks.";
   } else {
-    message = "No can do.";
+    return = "No can do.";
   }
-  return message
 }
 
 function ternaryCheckCity(city) {
