9554a58f8482c797c36d17dadb251780411c8570
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-04-21 23:29:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function switchOnCharmFromTip(tip) {
       return "Thank you so much";
     case 'not as generous':
       return "Thank you.";
-    default
-      return "Bye."
+    default:
+      return "Bye.";
   } 
 }
