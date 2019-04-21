52b8ca7b2ff49e400b926b6d5cde2dd47c83d741
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-04-21 22:54:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // Write your code in this file!
-function scuberGreetingForFeet(someValue) {
-  let feet
+
+
+function scuberGreetingForFeet() {
+  let feet;
   if (feet <= 400) {
-    feet = "This one is on me!";
+    feet = 'This one is on me!';
   } else if (1999 < feet && feet < 2500) {
     feet = "I will gladly take your thirty bucks.";
   } else {
